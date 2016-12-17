edbf4c85ec55b2bcb831b377ae0fc74e22987481
jake <jake@jakecoffman.com>
2016-12-16 20:18:09 -0600

    dev proxy

---
+++ proxy.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "net/http/httputil"
+    "log"
+    "net/url"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    fe, _ := url.Parse("http://localhost:8080")
+    be, _ := url.Parse("http://localhost:8444")
+
+    fep := httputil.NewSingleHostReverseProxy(fe)
+    bep := httputil.NewSingleHostReverseProxy(be)
+
+    r := gin.Default()
+    r.Any("/", func(ctx *gin.Context) {
+        fep.ServeHTTP(ctx.Writer, ctx.Request)
+    })
+    r.Any("/api/*path", func(ctx *gin.Context) {
+        ctx.Request.URL.Path = ctx.Param("path")
+        bep.ServeHTTP(ctx.Writer, ctx.Request)
+    })
+    log.Println("FRONTEND:", fe)
+    log.Println("BACKEND:", be)
+    log.Println("SERVING http://localhost:8111")
+    r.Run("0.0.0.0:8111")
+}
